550f16e868b3befc7bfb54d313b34e4c2ac43a99
GuriGuri <127086721+m960626@users.noreply.github.com>
2023-07-31 16:22:45 +0900

    Add files via upload

---
+++ src/main/java/org/example/dto/AddUserRequest.java
@@ -0,0 +1,12 @@
+package org.example.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class AddUserRequest {
+    private String email;
+    private String password;
+}
---
+++ src/main/java/org/example/service/UserDetailService.java
@@ -0,0 +1,19 @@
+package org.example.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.domain.User;
+import org.example.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public User loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException((email)));
+    }
+}
